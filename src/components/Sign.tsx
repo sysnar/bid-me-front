2c6113d900ce0da9875d89c336e1365f6730cb54
sysnar <sysn4r@gmail.com>
2021-12-25 02:51:53 +0900

    FIX sign in/out form responsive with change

--- src/components/Sign.tsx
+++ src/components/Sign.tsx
@@ -5,7 +5,7 @@ export function SignWrapper({ children }: WrapperProps) {
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto flex flex-wrap items-center justify-center">
-        <div className="lg:w-3/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col  w-full mt-10 md:mt-0">
+        <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col  w-full mt-10 md:mt-0">
           {children}
         </div>
       </div>

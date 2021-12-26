ca04459366e9b6a4e1cb550f2827fffd26f3f0ed
sysnar <sysn4r@gmail.com>
2021-12-26 13:40:40 +0900

    ADD signup axios request logic

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,17 +1,90 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import ConfirmBtn from "../components/ConfirmBtn";
 import { SignHeading, SignInput, SignWrapper } from "../components/Sign";
 
 export function SignUpPage() {
+  const [account, setAccount] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordChk: "",
+  });
+
+  const navigate = useNavigate();
+
+  const onChangeAccount = async (event: any) => {
+    setAccount({
+      ...account,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const onSubmitAccount = async (event: any) => {
+    try {
+      if (
+        (["name", "password"].includes(event.target.name) &&
+          event.key === "Enter") ||
+        event.target.name === "submitBtn"
+      ) {
+        event.preventDefault();
+        const response = await axios.post(
+          "http://localhost:4000/auth/signup",
+          account
+        );
+
+        console.log(response.data.statusCode);
+        if (response.data.statusCode === "OK") {
+          navigate(`/login`);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <SignWrapper>
       <SignHeading>회원가입</SignHeading>
-      <SignInput label="아이디" placeholer="아이디를 입력해주세요." />
-      <SignInput label="Email" placeholer="Email을 입력해주세요." />
-      <SignInput label="비밀번호" placeholer="비밀번호를 입력해주세요." />
-      <ConfirmBtn btnName="확인" />
-      <p className="text-xs text-gray-500 mt-3">
-        Literally you probably haven't heard of them jean shorts.
-      </p>
+      <SignInput
+        id="name"
+        name="name"
+        label="아이디"
+        placeholer="아이디를 입력해주세요."
+        onChangeAccount={onChangeAccount}
+        onKeyPress={onSubmitAccount}
+      />
+      <SignInput
+        id="email"
+        name="email"
+        label="Email"
+        placeholer="Email을 입력해주세요."
+        onChangeAccount={onChangeAccount}
+        onKeyPress={onSubmitAccount}
+      />
+      <SignInput
+        id="password"
+        name="password"
+        label="비밀번호"
+        placeholer="비밀번호를 입력해주세요."
+        onChangeAccount={onChangeAccount}
+        onKeyPress={onSubmitAccount}
+      />
+      <SignInput
+        id="passwordChk"
+        name="passwordChk"
+        label="비밀번호 확인"
+        placeholer="확인할 비밀번호를 입력해주세요."
+        onChangeAccount={onChangeAccount}
+        onKeyPress={onSubmitAccount}
+      />
+      <ConfirmBtn
+        name="submitBtn"
+        btnName="확인"
+        onSubmitAccount={onSubmitAccount}
+      />
+      <p className="text-xs text-gray-500 mt-3">SYSNAR. All rights reserved.</p>
     </SignWrapper>
   );
 }
